b749289306906ecf10abcf1485033a644e694efa
nalearning <119291808+nalearning@users.noreply.github.com>
2022-11-28 14:54:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast Menu")
+streamlit.title("My Mom's New Healthy Diner")
+streamlit.header("Breakfast Favourites")
 streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
 streamlit.text("🥗 Kale Spinach and Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Ranged Egg")
